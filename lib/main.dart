cd419436a343d65b2e1e6e686e34f1302d561215
Devair Luiz Moitim <dmoitim@gmail.com>
2020-03-23 03:47:23 -0300

    Criação do botão para calcular

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,26 @@ class HomePage extends StatelessWidget {
             label: "Álcool",
             ctrl: _alcoolCtrl,
           ),
+          Container(
+            margin: EdgeInsets.all(30),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(
+                60,
+              ),
+            ),
+            child: FlatButton(
+              child: Text(
+                "Calcular",
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 25,
+                    fontFamily: "Big Shoulders Display"),
+              ),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
