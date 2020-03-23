344440ccf9723c74294b588b5494f78c740e31ff
Devair Luiz Moitim <dmoitim@gmail.com>
2020-03-23 03:59:03 -0300

    Exibição do resultado

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,53 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Compensa utilizar álcool.",
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 40,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: EdgeInsets.all(30),
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(
+                      60,
+                    ),
+                  ),
+                  child: FlatButton(
+                    child: Text(
+                      "CALCULAR NOVAMENTE",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 25,
+                          fontFamily: "Big Shoulders Display"),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
           Input(
             label: "Gasolina",
             ctrl: _gasolinaCtrl,
@@ -49,7 +96,7 @@ class HomePage extends StatelessWidget {
             ),
             child: FlatButton(
               child: Text(
-                "Calcular",
+                "CALCULAR",
                 style: TextStyle(
                     color: Theme.of(context).primaryColor,
                     fontSize: 25,
